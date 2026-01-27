a23ac813d7984183bee9b2ee0ff6022ea9dec553
Antonija Mitić <mitic.pr13.2023@uns.ac.rs>
2026-01-27 01:24:44 +0100

    Initial commit

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a349d2157bf1c9a213517adbc386184a7f88aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60c750a014782daff9f2e6ecd312965759582563")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
