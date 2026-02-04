f4f75455e7fea6110705c217f9097b9f9dbc5927
donovina <nikola.lol.profesor@gmail.com>
2026-02-04 15:06:08 +0100

    Add .gitignore and remove bin/obj

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04e25529f152a3bd95c7664e2c12095ae37b161")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be80c1418fa09243de4d7c889924ab458ed7ce76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
