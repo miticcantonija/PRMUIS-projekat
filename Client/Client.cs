c04e25529f152a3bd95c7664e2c12095ae37b161
Antonija Mitić <mitic.pr13.2023@uns.ac.rs>
2026-02-04 04:28:42 +0100

    Tacka 3

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,39 +1,109 @@
-﻿using System.Net;
+﻿#pragma warning disable SYSLIB0011
+using System;
+using System.Net;
 using System.Net.Sockets;
+using System.IO;
 using System.Text;
-
+using System.Runtime.Serialization.Formatters.Binary;
+using Biblioteka.Modeli; // Ovde se nalaze Knjiga i Poruka
 
 namespace BibliotekaKlijent
 {
     public class Program
     {
-        private const string SERVER_IP = "127.0.0.1";
-        private const int TCP_PRISTUP_PORT = 6000;
-
         static void Main(string[] args)
         {
-            // 1) Kreiranje TCP klijentske utičnice 
-            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 1. Inicijalizacija TCP i UDP soketa
+            Socket klijentTcp = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket klijentUdp = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-            // 2) Server endpoint 
-            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(SERVER_IP), TCP_PRISTUP_PORT);
+            IPEndPoint serverTcpEP = new IPEndPoint(IPAddress.Loopback, 6000);
+            IPEndPoint serverUdpEP = new IPEndPoint(IPAddress.Loopback, 5000);
 
-            // 3) Connect() 
-            Console.WriteLine($"Povezujem se na server {SERVER_IP}:{TCP_PRISTUP_PORT} ...");
-            clientSocket.Connect(serverEndPoint);
-            Console.WriteLine("Prijava uspešna (TCP konekcija uspostavljena).");
+            try
+            {
+                // 2. TCP Povezivanje i prijem ID-a
+                Console.WriteLine("Povezujem se na server...");
+                klijentTcp.Connect(serverTcpEP);
 
-            // 4) Prijem ID-a (Receive) 
-            byte[] buffer = new byte[256];
-            int bytesReceived = clientSocket.Receive(buffer);
-            string idStr = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                byte[] idBuf = new byte[256];
+                int primljeno = klijentTcp.Receive(idBuf);
+                int mojID = int.Parse(Encoding.UTF8.GetString(idBuf, 0, primljeno));
 
-            // 5) "Čuvanje" ID-a (za sad u memoriji; ispis) 
-            Console.WriteLine($"Dodeljen ID klijenta: {idStr}");
-            Console.WriteLine("ID sačuvan lokalno (trenutno u memoriji aplikacije).");
+                Console.WriteLine($"[USPEH] Prijavljen na server! Dodeljen ID: {mojID}");
 
-            clientSocket.Close();
-            Console.WriteLine("Kraj.");
+                while (true)
+                {
+                    Console.WriteLine("\n========== KLIJENT MENI ==========");
+                    Console.WriteLine("1 - Pošalji novu knjigu (TCP)");
+                    Console.WriteLine("2 - Proveri da li knjiga postoji (UDP)");
+                    Console.WriteLine("3 - Preuzmi listu svih knjiga (UDP)");
+                    Console.WriteLine("0 - Izlaz");
+                    Console.Write("Izbor: ");
+                    string izbor = Console.ReadLine();
+
+                    if (izbor == "1")
+                    {
+                        // SLANJE KNJIGE PREKO TCP-A
+                        Knjiga k = new Knjiga();
+                        Console.Write("Naslov: "); k.Naslov = Console.ReadLine();
+                        Console.Write("Autor: "); k.Autor = Console.ReadLine();
+                        Console.Write("Količina: ");
+                        k.Kolicina = int.TryParse(Console.ReadLine(), out int kol) ? kol : 1;
+
+                        Poruka p = new Poruka { KlijentID = mojID, KnjigaPodaci = k };
+
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            BinaryFormatter bf = new BinaryFormatter();
+                            bf.Serialize(ms, p);
+                            klijentTcp.Send(ms.ToArray());
+                        }
+                        Console.WriteLine("[TCP] Knjiga poslata sa tvojim ID-em.");
+                    }
+                    else if (izbor == "2")
+                    {
+                        // UPIT ZA KNJIGU PREKO UDP-A
+                        Console.Write("Unesite naslov za proveru: ");
+                        string naslov = Console.ReadLine();
+                        byte[] data = Encoding.UTF8.GetBytes("PROVERA:" + naslov);
+                        klijentUdp.SendTo(data, serverUdpEP);
+
+                        // Prijem odgovora
+                        byte[] buffer = new byte[2048];
+                        EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+                        int n = klijentUdp.ReceiveFrom(buffer, ref remote);
+                        Console.WriteLine("\n[UDP ODGOVOR]: " + Encoding.UTF8.GetString(buffer, 0, n));
+                    }
+                    else if (izbor == "3")
+                    {
+                        // UPIT ZA LISTU PREKO UDP-A
+                        byte[] data = Encoding.UTF8.GetBytes("LISTA:SVE");
+                        klijentUdp.SendTo(data, serverUdpEP);
+
+                        // Prijem odgovora
+                        byte[] buffer = new byte[4096];
+                        EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+                        int n = klijentUdp.ReceiveFrom(buffer, ref remote);
+                        Console.WriteLine("\n[LISTA KNJIGA]:\n" + Encoding.UTF8.GetString(buffer, 0, n));
+                    }
+                    else if (izbor == "0")
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n[GREŠKA]: " + ex.Message);
+            }
+            finally
+            {
+                klijentTcp.Close();
+                klijentUdp.Close();
+                Console.WriteLine("Konekcije zatvorene. Pritisnite bilo koji taster za kraj.");
+                Console.ReadKey();
+            }
         }
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,152 +1,161 @@
-﻿using System.Net;
+﻿#pragma warning disable SYSLIB0011
+using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
+using System.IO;
 using System.Text;
-
-
+using System.Runtime.Serialization.Formatters.Binary;
+using Biblioteka.Modeli; // Namespace tvoje zajedničke biblioteke
 
 namespace BibliotekaServer
 {
-    public class Knjiga
-    {
-        
-        public string Naslov { get; set; } = "";
-        public string Autor { get; set; } = "";
-        public int Kolicina { get; set; }
-    }
-
     public class Program
     {
-        // Portovi (mozes promeniti)
-        private const int UDP_INFO_PORT = 5000;
-        private const int TCP_PRISTUP_PORT = 6000;
+        private const int UDP_PORT = 5000;
+        private const int TCP_PORT = 6000;
 
-        private static readonly List<Knjiga> _knjige = new();
-        private static int _nextClientId = 10000; // “višecifren” ID
+        private static List<Knjiga> _knjige = new List<Knjiga>();
+        private static List<Socket> _klijenti = new List<Socket>();
+        private static int _nextClientId = 10550;
 
         static void Main(string[] args)
         {
-            // 1) UDP INFO socket 
-            Socket udpInfoSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            udpInfoSocket.Bind(new IPEndPoint(IPAddress.Any, UDP_INFO_PORT)); // Bind primer :contentReference[oaicite:6]{index=6}
+            // 1. UDP SOKET - Za brze upite (provera i lista)
+            Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(new IPEndPoint(IPAddress.Any, UDP_PORT));
+            udpSocket.Blocking = false;
 
-            // Ispis IP/port (LocalEndPoint) 
-            var udpLocal = udpInfoSocket.LocalEndPoint as IPEndPoint;
-            Console.WriteLine($"UDP INFO utičnica aktivna na IP: {udpLocal?.Address}, PORT: {udpLocal?.Port}");
+            // 2. TCP LISTEN SOKET - Za prihvatanje novih klijenata
+            Socket tcpListen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpListen.Bind(new IPEndPoint(IPAddress.Any, TCP_PORT));
+            tcpListen.Listen(10);
+            tcpListen.Blocking = false;
 
-            // 2) TCP PRISTUP socket
-            Socket tcpListenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            tcpListenSocket.Bind(new IPEndPoint(IPAddress.Any, TCP_PRISTUP_PORT));
-            tcpListenSocket.Listen(int.MaxValue); 
-
-            var tcpLocal = tcpListenSocket.LocalEndPoint as IPEndPoint;
-            Console.WriteLine($"TCP PRISTUPNA utičnica aktivna na IP: {tcpLocal?.Address}, PORT: {tcpLocal?.Port}");
-
-            // 3) Dodavanje knjiga 
-            Console.WriteLine();
-            Console.WriteLine("========= Server biblioteke =========");
-            Console.WriteLine("Komande:");
-            Console.WriteLine("  1 - Dodaj knjigu");
-            Console.WriteLine("  2 - Prikaži sve knjige");
-            Console.WriteLine("  3 - Čekaj prijavu klijenta (TCP Accept)");
-            Console.WriteLine("  0 - Izlaz");
-            Console.WriteLine();
+            Console.WriteLine("========== SERVER BIBLIOTEKE ==========");
+            Console.WriteLine($"UDP Info port: {UDP_PORT}");
+            Console.WriteLine($"TCP Pristupni port: {TCP_PORT}");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Komande: [1] Dodaj rucno | [2] Lista | [0] Izlaz\n");
 
             while (true)
             {
-                Console.Write("Unesi komandu: ");
-                string? cmd = Console.ReadLine();
+                // Priprema liste za Select (osluškujemo listen soket, udp i sve povezane klijente)
+                List<Socket> readList = new List<Socket> { tcpListen, udpSocket };
+                readList.AddRange(_klijenti);
 
-                if (cmd == "0")
-                    break;
+                // Čekamo 0.1s na mrežne događaje
+                if (readList.Count > 0)
+                    Socket.Select(readList, null, null, 100000);
 
-                if (cmd == "1")
+                foreach (Socket s in readList)
                 {
-                    DodajKnjigu();
-                }
-                else if (cmd == "2")
-                {
-                    PrikaziKnjige();
-                }
-                else if (cmd == "3")
-                {
-                    PrihvatiPrijavuKlijenta(tcpListenSocket);
-                }
-                else
-                {
-                    Console.WriteLine("Nepoznata komanda.");
+                    if (s == tcpListen) // NOVI KLIJENT SE POVEZUJE
+                    {
+                        Socket noviKlijent = s.Accept();
+                        noviKlijent.Blocking = false;
+                        _klijenti.Add(noviKlijent);
+
+                        // Slanje ID-a klijentu kao string
+                        byte[] idBytes = Encoding.UTF8.GetBytes(_nextClientId.ToString());
+                        noviKlijent.Send(idBytes);
+
+                        Console.WriteLine($"\n[TCP] Povezan klijent. Dodeljen ID: {_nextClientId}");
+                        _nextClientId++;
+                    }
+                    else if (s == udpSocket) // UDP UPIT (PROVERA ILI LISTA)
+                    {
+                        EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                        byte[] buffer = new byte[1024];
+                        int rec = s.ReceiveFrom(buffer, ref remoteEP);
+
+                        string zahtev = Encoding.UTF8.GetString(buffer, 0, rec);
+                        string odgovor = ObradiUdpZahtev(zahtev);
+
+                        byte[] odgovorBytes = Encoding.UTF8.GetBytes(odgovor);
+                        s.SendTo(odgovorBytes, remoteEP);
+                    }
+                    else // POSTOJEĆI KLIJENT ŠALJE KNJIGU (TCP)
+                    {
+                        try
+                        {
+                            byte[] buffer = new byte[4096];
+                            int primljeno = s.Receive(buffer);
+                            if (primljeno == 0) throw new Exception("Klijent se odjavio");
+
+                            using (MemoryStream ms = new MemoryStream(buffer, 0, primljeno))
+                            {
+                                BinaryFormatter bf = new BinaryFormatter();
+                                Poruka p = (Poruka)bf.Deserialize(ms);
+
+                                _knjige.Add(p.KnjigaPodaci);
+                                Console.WriteLine($"\n[TCP] Klijent {p.KlijentID} dodao knjigu: {p.KnjigaPodaci.Naslov}");
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("\n[TCP] Klijent je prekinuo vezu.");
+                            s.Close();
+                            _klijenti.Remove(s);
+                            break;
+                        }
+                    }
                 }
 
-                Console.WriteLine();
+                // RAD SA KONZOLOM SERVERA
+                if (Console.KeyAvailable)
+                {
+                    var kljuc = Console.ReadKey(true).Key;
+                    if (kljuc == ConsoleKey.D1) DodajKnjiguLokalno();
+                    else if (kljuc == ConsoleKey.D2) PrikaziSveKnjige();
+                    else if (kljuc == ConsoleKey.D0) return;
+                }
             }
-
-            // Zatvaranje utičnica
-            udpInfoSocket.Close();
-            tcpListenSocket.Close();
         }
 
-        private static void DodajKnjigu()
+        // --- POMOĆNE METODE ---
+
+        private static string ObradiUdpZahtev(string zahtev)
         {
-            Console.Write("Naslov: ");
-            string naslov = Console.ReadLine() ?? "";
-
-            Console.Write("Autor: ");
-            string autor = Console.ReadLine() ?? "";
-
-            Console.Write("Količina: ");
-            int kolicina;
-            while (!int.TryParse(Console.ReadLine(), out kolicina))
+            if (zahtev.StartsWith("PROVERA:"))
             {
-                Console.Write("Unesi ceo broj za količinu: ");
+                string naslov = zahtev.Substring(8).Trim();
+                var k = _knjige.Find(x => x.Naslov.Equals(naslov, StringComparison.OrdinalIgnoreCase));
+
+                if (k != null && k.Kolicina > 0)
+                    return $"Knjiga '{k.Naslov}' postoji. Kolicina: {k.Kolicina}.";
+
+                return "Knjiga nije pronadjena ili je kolicina 0.";
             }
-
-            _knjige.Add(new Knjiga
+            else if (zahtev == "LISTA:SVE")
             {
-                Naslov = naslov,
-                Autor = autor,
-                Kolicina = kolicina
-            });
+                var dostupne = _knjige.FindAll(x => x.Kolicina > 0);
+                if (dostupne.Count == 0) return "Biblioteka je prazna.";
 
-            Console.WriteLine("Knjiga dodata u listu.");
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("Dostupne knjige na stanju:");
+                foreach (var k in dostupne)
+                    sb.AppendLine($"- {k.Naslov} ({k.Autor}) | Kolicina: {k.Kolicina}");
+
+                return sb.ToString();
+            }
+            return "Nepoznata UDP komanda.";
         }
 
-        private static void PrikaziKnjige()
+        private static void DodajKnjiguLokalno()
         {
-            if (_knjige.Count == 0)
-            {
-                Console.WriteLine("Lista knjiga je prazna.");
-                return;
-            }
-
-            Console.WriteLine("=== Knjige (server) ===");
-            for (int i = 0; i < _knjige.Count; i++)
-            {
-                var k = _knjige[i];
-                Console.WriteLine($"{i + 1}. {k.Naslov} - {k.Autor} (Količina: {k.Kolicina})");
-            }
+            Console.WriteLine("\n--- Rucni unos knjige ---");
+            Console.Write("Naslov: "); string n = Console.ReadLine();
+            Console.Write("Autor: "); string a = Console.ReadLine();
+            _knjige.Add(new Knjiga { Naslov = n, Autor = a, Kolicina = 5 });
+            Console.WriteLine("Knjiga uspesno dodata.");
         }
 
-        private static void PrihvatiPrijavuKlijenta(Socket tcpListenSocket)
+        private static void PrikaziSveKnjige()
         {
-            Console.WriteLine("Čekam TCP prijavu klijenta...");
-
-            // Accept() 
-            Socket acceptedSocket = tcpListenSocket.Accept();
-
-            // Ispis udaljene adrese (RemoteEndPoint) 
-            var remote = acceptedSocket.RemoteEndPoint as IPEndPoint;
-            Console.WriteLine($"Klijent povezan sa: {remote?.Address}:{remote?.Port}");
-
-            // Generiši jedinstveni višecifreni ID 
-            int clientId = _nextClientId++;
-            Console.WriteLine($"Dodeljujem klijentu ID: {clientId}");
-
-            // Pošalji ID klijentu preko TCP (Send) 
-            byte[] idBytes = Encoding.UTF8.GetBytes(clientId.ToString());
-            acceptedSocket.Send(idBytes);
-
-            // Po želji: zatvori konekciju (u zadatku 2 nije traženo dalje)
-            acceptedSocket.Close();
-            Console.WriteLine("ID poslat klijentu. Konekcija zatvorena.");
+            Console.WriteLine("\n--- Stanje u biblioteci ---");
+            if (_knjige.Count == 0) Console.WriteLine("Nema knjiga.");
+            else _knjige.ForEach(k => Console.WriteLine($"[{k.Kolicina}] {k.Naslov} - {k.Autor}"));
         }
     }
 }
