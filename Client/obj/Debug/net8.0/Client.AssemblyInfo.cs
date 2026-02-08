a08b12f7bb76778789c5db761852bd3efce22252
donovina <nikola.lol.profesor@gmail.com>
2026-02-09 00:00:04 +0100

    Remove remaining obj files from tracking

--- Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fb9c9fcca1c3035c4f5780cfffb7360652cb61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb17c8fe95b3d035cebeb2b138769f4cce52b77c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
